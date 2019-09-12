1f1cf072b1b8fac45e3801643f92d5bfa23047b2
Eliazar Alejandro <liazcortez@gmail.com>
2019-09-12 14:44:42 -0500

    se agrego material y funciones de copiar y limpiar

--- src/app/components/remove-text/remove-text.component.ts
+++ src/app/components/remove-text/remove-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-remove-text',
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RemoveTextComponent implements OnInit {
 
+  @ViewChild('texto',{static:false}) texto: ElementRef;
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  cambiartexto(){
+    var stringfinal:any = [];
+    let string :any = this.texto.nativeElement.value;
+    //  stringfinal = string.replace(/\n/gi, ' '); .replace(/\.  /gi, '. \n\n');
+    stringfinal = string.replace(/\.\n/gi, '#####');
+    setTimeout(() => {
+        stringfinal = stringfinal.replace(/\n/gi, ' ').replace(/#####/gi, '.\n\n');
+        console.log(stringfinal);
+        this.texto.nativeElement.value = stringfinal;
+    }, 50);
+
+    this.texto.nativeElement.value = stringfinal;
+  }
+
+  copiartexto(){
+
+  /* Select the text field */
+  this.texto.nativeElement.select();
+  this.texto.nativeElement.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + this.texto.nativeElement.value);
+
+  }
+  limpiartexto(){
+    this.texto.nativeElement.value = '';
+
+  }
 }
